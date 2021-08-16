6a57e34631a1fb15f6d4e3bd18f0d50779cf68b7
ibmpariwesh <ibmpariwesh@gmail.com>
2021-08-16 14:17:34 +0530

    JDBC CRUD ops

--- src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
+++ src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBCDemo {
 	public static void main(String[] args) {
@@ -15,17 +17,30 @@ public class JDBCDemo {
 		try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 				Statement statement = connection.createStatement();) {
 //			create(statement); // create
-			retrieve(statement);
+//			retrieve(statement);
+//			update(statement);
+//			delete(statement);
+			List<String> regions = retriveWithCondition(statement, "A");//regions starting with 'A'
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
+	private static List<String> retriveWithCondition(Statement statement, String string) {
+		return null;
+	}
+
+	private static void update(Statement statement) {
+		
+	}
+
 	private static void retrieve(Statement statement) throws SQLException {
 		ResultSet resultSet = statement.executeQuery("SELECT * from regions");
+		List<String> regions = new ArrayList<String>();
 		while (resultSet.next()) {
-			System.out.println(resultSet.getInt(1));
+			System.out.println(resultSet.getObject(1));
 			System.out.println(resultSet.getNString("REGION_NAME"));
+			regions.add(resultSet.getNString("REGION_NAME"));
 		}
 	}
 
